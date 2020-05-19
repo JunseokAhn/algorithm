983afb4d2f3bde78c632ea4a6875dbfb2f1d8a8c
JunseokAhn <57289429+JunseokAhn@users.noreply.github.com>
2020-05-19 11:52:37 +0900

    Add counting_sort

---
+++ src/algorithm/counting_sort.java
@@ -0,0 +1,34 @@
+package algorithm;
+
+public class counting_sort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int arr[] = { 5, 5, 3, 3, 3, 1, 1, 4, 1, 2, 1, 3, 4, 2, 2, 3 };
+
+		int count[] = new int[5];
+
+		// count 배열 0으로 초기화
+		for (int i : count) {
+			i = 0;
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			count[arr[i] - 1]++;
+		}
+
+		int sum = 0;
+
+		for (int i = 0; i < count.length; i++) {
+			for (int j = 0; j < count[i]; j++) {
+				
+			}
+			sum += count[i]-1;
+		}
+
+		for (int i : arr)
+			System.out.print(i);
+	}
+
+}
